da07d6c3b1be6c6b71b484ff7abf23c7c08fc381
Lyriom <josueriera23@hotmail.com>
2023-10-26 13:38:24 -0500

    Se sube el archivo de Calculadora.java con sus metodos Calcu.java

---
+++ src/Calcu.java
@@ -0,0 +1,71 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Calcu {
+    public double num1;
+    public double num2;
+
+    public double res;
+
+
+
+    public void Suma(double num1, double num2)
+    {
+        System.out.println( "Ingrese un numero");
+        Scanner scanner = new Scanner(System.in);
+        num1= scanner.nextDouble();
+        System.out.println( "Ingrese un segudno numero");
+        Scanner scanner2 = new Scanner(System.in);
+        num2= scanner.nextDouble();
+        res = num1 +num2;
+
+
+    }
+
+    public void Resta(double num1, double num2)
+    {
+        System.out.println( "Ingrese un numero");
+        Scanner scanner = new Scanner(System.in);
+        num1= scanner.nextDouble();
+        System.out.println( "Ingrese un segudno numero");
+        Scanner scanner2 = new Scanner(System.in);
+        num2= scanner.nextDouble();
+        res = num1-num2;
+
+
+    }
+
+    public void Multiplicacion(double num1, double num2)
+    {
+        System.out.println( "Ingrese un numero");
+        Scanner scanner = new Scanner(System.in);
+        num1= scanner.nextDouble();
+        System.out.println( "Ingrese un segudno numero");
+        Scanner scanner2 = new Scanner(System.in);
+        num2= scanner.nextDouble();
+        res = num1*num2;
+
+
+    }
+    public void Division(double num1, double num2)
+    {
+        System.out.println( "Ingrese un numero");
+        Scanner scanner = new Scanner(System.in);
+        num1= scanner.nextDouble();
+        System.out.println( "Ingrese un segudno numero");
+        Scanner scanner2 = new Scanner(System.in);
+        num2 = scanner.nextDouble();
+
+        if ( num2 == 0){
+            System.out.println("no se puede hacer una division entre 0");
+        }else {
+            res = num1/num2;
+        }
+
+    }
+public double getRes(){
+
+        return res;
+}
+
+}
---
+++ src/Calculadora.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        int opcion;
+        System.out.println("Escoja 1 suma");
+        System.out.println("Escoja 2 resta");
+        System.out.println("Escoja 3 multiplicacion");
+        System.out.println("Escoja 4 division");
+        Scanner scanner = new Scanner(System.in);
+        opcion= scanner.nextInt();
+
+        switch (opcion) {
+            case 1:
+                Calcu suma =new Calcu();
+                suma.Suma(suma.num1, suma.num2);
+                System.out.println(suma.res);
+                break;
+            case 2:
+                Calcu resta =new Calcu();
+                resta.Resta(resta.num1, resta.num2);
+                System.out.println(resta.res);
+                break;
+            case 3:
+                Calcu multi =new Calcu();
+                multi.Multiplicacion(multi.num1, multi.num2);
+                System.out.println(multi.res);
+                break;
+            case 4:
+                Calcu div =new Calcu();
+                div.Division(div.num1, div.num2);
+                System.out.println(div.res);
+                break;
+            default:
+        }
+    }
+}
